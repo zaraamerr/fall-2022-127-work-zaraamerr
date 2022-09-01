5dd6b3b63a7a49abaf2af7e958ea376152c8cb6f
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-09-01 13:37:02 +0000

    added a file called t2.py and modified t1.py

--- turtles/t1.py
+++ turtles/t1.py
@@ -2,8 +2,14 @@ import turtle
 
 wn= turtle.Screen()
 
-# draw a square #
+# create the first turtle
+# into the variable crush
+#and make crush draw a square
+
 crush= turtle.Turtle()
+crush.up()
+crush.goto(100,100)
+crush.down()
 crush.forward (50)
 crush.right(90)
 crush.forward(50)
@@ -13,6 +19,23 @@ crush.right(90)
 crush.forward(50)
 crush.right(90)
 
+#create a second turtle
+#into the variable squirt
+#and make squirt draw a triangle
+
+squirt= turtle.Turtle()
+
+squirt.up()
+squirt.goto(50,100)
+squirt.down()
+squirt.color("red")
+squirt.width(5)
+squirt.forward(30)
+squirt.right(120)
+squirt.forward(30)
+squirt.right(120)
+squirt.forward(30)
+squirt.right(120)
 
 wn.exitonclick()
 wn.mainloop()
---
+++ turtles/t2.py
@@ -0,0 +1,11 @@
+for i in range (4):
+    print(i)
+print("----------------------")
+for number in range (5,10):
+  print(number)
+print ("---------------------")
+for name in ['pencil, chicken, thomas']:
+  print(name)
+print(name.upper())
+
+print("The loop is done!")
