b63e542b14001dff3827f49871515adb913ac2cf
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-09-01 12:59:05 +0000

    added a folder named turtles with a file called t1.py

---
+++ turtles/t1.py
@@ -0,0 +1,18 @@
+import turtle
+
+wn= turtle.Screen()
+
+# draw a square #
+crush= turtle.Turtle()
+crush.forward (50)
+crush.right(90)
+crush.forward(50)
+crush.right(90)
+crush.forward(50)
+crush.right(90)
+crush.forward(50)
+crush.right(90)
+
+
+wn.exitonclick()
+wn.mainloop()
