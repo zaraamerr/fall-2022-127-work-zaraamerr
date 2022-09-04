e0160a7897727a769f5104195c0a3c16929865ca
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-09-04 18:39:01 +0000

    added folder named chapter 4 with files named one.py, three.py, and seven.py

---
+++ chapter_4/seven.py
@@ -0,0 +1,21 @@
+import turtle
+
+wn = turtle.Screen()
+pirate = turtle.Turtle()
+
+# move turtle forward on the screen to capture # the entire path on the screen
+pirate.penup()
+pirate.forward(100)
+
+# the drunk pirate's path
+pirate.pendown()
+for angle in [160, -43, 270, -97, -43, 200, -940, 17, -86]:
+  # positive angles are counterclockwise and
+  # negative angles are clockwise
+    pirate.left(angle)
+    pirate.forward(100)
+
+# the current heading of the drunk pirate
+print("The drunk pirate's final heading was", pirate.heading())
+
+wn.exitonclick()
