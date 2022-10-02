8b358c8ef72b52938012e14b1d4efc262e17ec0e
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-10-02 20:15:52 +0000

    added new folder called chapter-10 with file called list1.py

---
+++ chapter-10/list1.py
@@ -0,0 +1,14 @@
+# exercises 2 and 3 from chapter 10
+
+# create an empty list called my list
+myList=[]
+# append the first 3 items to the list
+myList.append(76)
+myList.append(92.3)
+myList.append("hello")
+# concatenate the last 3 items to the list
+myList= myList + [True]
+myList= myList + [4]
+myList= myList + [76]
+
+print (myList)
