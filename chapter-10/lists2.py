2488518a9f26f3ddce84804e83c2704579d13d7b
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-10-03 13:27:05 +0000

    modified lists2.py and added new folder called lists

--- chapter-10/lists2.py
+++ chapter-10/lists2.py
@@ -1,29 +1,27 @@
-#exercise 4 chapter 10
+#exercise 4 and 6 chapter 10
 
-import random
+def avg(n):
+    # calculate the sum of the items
+    sum = 0
+    for item in n:
+        sum = sum + item
 
-def average (numlist):
-  """
-  parameters: numlist
-  returns: avg of nums
+    # divide by the number of items
+    average = sum / len(n)
+    return average
 
-  """
-  total= 0
-  for num in numlist:
-    total= total + num
-    result = total/len(numlist)
-    return result
+def sum_of_squares(n):
+    sum = 0;
+    for item in n:
+        sum = sum + item*item
+    return sum
 
-#exercise 6 chapter 10
 
-xs=[]
-for i in range (3):
-  xs.append(random.randint(0,50))
-def sum_of_squares(xs):
-  sum_of_squares=0
-  for i in (xs):
-    squared= i*i
-    sum_of_squares += squared
-    return sum_of_squares
+grades = [90,95,100,90]
+print("grades:",grades)
+average=avg(grades)
+print("Average:",average)
 
-print (sum_of_squares(xs))
+n = [3,4,5]
+print("n:",n)
+print("sum of squares:",sum_of_squares(n))
---
+++ lists/listdemos.py
@@ -0,0 +1,76 @@
+s= "this is a string"
+elist=[]
+l1=[1,2,3,4,5,6,7,8]
+print (l1)
+l2=["hello", "good morning", "ciao", "adios"]
+print (l2)
+l3=["hello", 3, 4, 5, "good night", 7, 9, 11]
+print (l3)
+l4=["one", 45, ['a', 'b', 'c'], "nice", [10,12,13]]
+print (l4)
+slice= l1[1:5]
+print (slice)
+longer_string= s+s
+print (longer_string)
+longer_list= l1+l4
+print (longer_list)
+
+#strings are inmutable 
+# instead you can do this
+
+new_string= s[:5]+ 'I' +s[6:]
+print (new_string)
+
+#we can also change lists directly
+l4[4]: 99999
+print(l4)
+
+#you can change a list's elements
+#but that isn't encouraged
+#since you don't return anything
+#which can be confusing to programmers
+
+#this is slightly better
+def change_in_place(l, index, new_value):
+   l[index]= new_value
+   return
+print (l1)
+change_in_place(l1, 4, 23)
+print (l1)
+
+print ("---------------------------------")
+
+#this is an example of aliasing (aka shallow copy)
+# it can be powerful but you have to be careful
+# and make sure you're not changing any lists
+# that you don't wanna change
+
+l2=l1
+print("l1:", l1)
+print ("l2:", l2)
+l1[4]=999
+print ("l1:", l1)
+print ("l2:", l2)
+
+#this is how you would usually do
+# a function to change part of a list
+# when you want to follow the functional paradigm
+
+def change_value(l, index, value):
+  result=[]
+  for item in l:
+    #notice we can stick a variable
+    #like item in [] to make it a list
+    # then add it to a list to result which is also
+    # a list so result = result + [item]
+    # or we can call the list method and append item
+    #result.append(item)
+    #result= l.copy()
+    result=1[:]
+    result[index]=value
+    return result
+
+l2  = change_value(l1,4,1111)
+print("l1:",l1)
+print("l2:",l2)
+    
