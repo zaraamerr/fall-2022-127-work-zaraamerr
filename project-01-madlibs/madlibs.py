eb16cfe745d6ff96efb7a592cd785dd74ddd90cc
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-10-17 13:26:16 +0000

    modified madlibs.py and added new txt file for mad libs story

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -11,4 +11,31 @@
 #adjectives:
 #adverbs:
 #animals:
-#
+
+
+#open data file: open()
+#read the entire data file: .read()
+#read text line by line: .readline()
+
+f = open('madlibsstory.txt')
+data = f.read()
+words_from_data = data.split()
+lines_from_data = data.split('\n')
+
+print (words_from_data)
+print(lines_from_data)
+
+f.close()
+f = open('madlibsstory.txt')
+lines = f.readlines()
+print (lines)
+# we can strip out the newlines from
+# lines
+
+# adding a line without a newline
+# just to show
+lines.append("This line has no newline")
+stripped = []
+for line in lines:
+    stripped.append(line.strip())
+print (lines)
