3f0fc5e4f743380b91471899986916924cd36a8f
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-10-30 02:12:21 +0000

    fixed mode function

--- mode/modes.py
+++ mode/modes.py
@@ -1,3 +1,4 @@
+
 def findLargest(l):
     current_large = l[0]
     for item in l[1:]:
@@ -5,17 +6,17 @@ def findLargest(l):
             current_large = item
     return current_large
 #test function
-l=[8,6,-4, 56]
+l=[5,8,9,0,34,76]
 result=findLargest(l)
 print ("This is the largest number:", result)
-    
   
 def freq(l,v):
-  frequency=0
-  for item in l:
-    if item==v:
-      frequency= frequency+1
-  return frequency
+  #frequency=0
+  #for item in l:
+    #if item==v:
+      #frequency= frequency+1
+  #return frequency
+  return len([x for x in l if x == v])
 
 #test the function
 l= [1,1,1,1,1,1,1,4,4,4,4,3,3,3,2,2,2,2,5,5,5,1,1,1,1,1,4,4,4,4]
@@ -30,15 +31,25 @@ l= [3,4,4,4,4,5,5,5,5,5,7,7,7,18,18,18,3,3,3,3,1,1]
 frequency=len([x for x in l if x==5])
 print (frequency)
 
+def mode(l):
+    """
+    Returns a mode of the dataset, that is
+    the value that appears most frequently
+    if multiple values appear the same # of times and are
+    most frequent, return any of them.
+    Ex: mode([5,4,5,6,7,8,5,4]) --> 5 since 5 appears the most
+    mode([5,5,5,4,4,4,2,2,7,7,8,8,9]) --> return 5 or 4 since
+    both of those values appear 3 times which is the most
+    """
+    modeSoFar = l[0]
+    freqSoFar = freq(l,modeSoFar)
+    for item in l[1:]:
+        if freq(l,item) > freqSoFar:
+            modeSoFar = item
+            freqSoFar = freq(l,item)
+            
+    return modeSoFar
 
-#----------------------------------------------
-#import random
-#def buildRandomList(size,maxvalue):
-  #result=[]
-  #for x in range (size):
-    #result.append(random.randrange(maxvalue))
-  #result= [random.randrange(maxvalue) for x in range(size)]
-  #return result
-#print (result)
-
-  
+l= [4,3,4,2,3,2,2,2,2,2,4,4,4,4,4,5,5,5,5,3,3]
+result= mode(l)
+print (result)
