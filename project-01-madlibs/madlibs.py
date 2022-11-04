f07d1226c52b88da38f3656789377c9b8d01c235
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-11-04 00:11:07 +0000

    created new madlibs story

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -1,18 +1,18 @@
-# mad libs project !!!
+# mad libs project
 #EXTRA 1: Instead of specifying the sentences or story to convert, write a story in a file and read it from your program. Make sure to include the file in your repo and that your program reads it correctly.
 #EXTRA 2: Add some replacements that are unique, that is, the first time you see them you select on randomly but then you keep reusing that replacement.
+#EXTRA 3: Pay attention to letter case. That is, if you replace a word at the beginning of a sentence, it should be capitalized, otherwise, lowercase. This is except in the case of proper nouns which should always be capitalized.
 
-#parts of speech
+#first, let's establish the parts of speech and their respective lists
 
-present_verbs=["mixes", "pours", "launches", "hurls", "sips", "gobbles"]
-verbs=["eat", "sleep on", "destroy", "love", "run", "stomp on"]
-verbs_ending_in_ing=["baking", "constructing", "stomping on", "laughing at", "fighting"]
-nouns=["salt", "sugar", "shoe", "broccoli", "computer", "skyscraper", "paint", "pan"]
-plural_nouns=["eggs", "phones", "books", "houses", "cups", "tablesp", "friends", "siblings"]
-character_names=["Paulina", "Fatima", "Remi", "Mr.Rogers", "Bugs Rabbit"]
-adjectives=["awesome", "delicious", "horrific", "lovely", "speedy", "beautiful","necessary", "useless", "silly", "goofy", "goopy", "watery" ]
+adjectiveList=["joyous", "fun", "breezy", "hot", "cold", "loud", "quiet", "peaceful", "chaotic"]
+nounList=["river", "lake", "mountain", "hotel", "beach", "fire department", "ditch", "raveen", "valley"]
+verb_ing_List=["hiking", "crying", "laughing", "hanggliding", "coding", "swimming", "hurling", "running"]
+nameList=["Tommy", "Jessie", "Lora", "Michael Jackson", "Drake", "Josh", "Betsy"]
 
-#open the data file, read it, and split it into words so it's easy to iterate through the text and substitute the parts of speech into the text
+#next, let's open the data file
+
+#, read it, and split it into words so it's easy to iterate through the text and substitute the parts of speech into the mad libs story.
 
 f = open('madlibsstory.txt')
 storywords = f.read()
@@ -21,28 +21,3 @@ print (storywords)
 #substituting randomized parts of speech into the mad libs story
 
 import random
-
-def substitute(story, present_verbs, verbs, verbs_ing, nouns, plural_nouns, character_names, adjectives):
-  #make the character's name a constant in the story
-  character= random.choice(character_names)
-  story= story.replace('<CHARACTER>', character)
-  storywords= story.split()
-  for i in storywords:
-  # now the other parts of speech
-    if i.find("<ADJECTIVE>"): #find <ADJECTIVE>
-      storywords[(storywords.index(i))]= random.choice(plural_nouns)#sub <ADJECTIVE> with an adjective from the list
-    if i.find ("<PLRLNOUN>"): #find <PLRLNOUN>
-      storywords[(storywords.index(i))]= random.choice(plural_nouns) #sub <PLRLNOUN> with a plural noun from the list
-    if i.find ("<NOUN>"): #find <NOUN>
-      storywords[(storywords.index(i))]= random.choice(nouns) #sub <NOUN> with a noun from the list
-    if i.find ("<VERB-ING>"): #find <VERB-ING>
-      storywords[(storywords.index(i))]= random.choice(verbs_ending_in_ing) #sub <VERB-ING> with a verb ending in -ing from the list
-    if i.find ("<VERB>"): #find <VERB>
-      storywords[(storywords.index(i))]= random.choice(verbs) #sub <VERB> with a verb from the list
-    if i.find ("<PRSNTVERB>"): #find <PRSNTVERB>
-      storywords[(storywords.index(i))]= random.choice(present_verbs) #sub <PRSNTVERB> with a present verb from the list
-      return "".join(story)
-
-print (substitute(storywords,present_verbs,verbs, verbs_ending_in_ing, nouns, plural_nouns, character_names, adjectives))
-    
-    
