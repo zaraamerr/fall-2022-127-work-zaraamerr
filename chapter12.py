cda4683da0adefa026c57446a4549a6e2bfab76e
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-11-07 01:08:13 +0000

    fixed exercise 1 indentation error

--- chapter12.py
+++ chapter12.py
@@ -1,15 +1,19 @@
 #exercise 1: Write a program that allows the user to enter a string. It then prints a table of the letters of the alphabet in alphabetical order which occur in the string together with the number of times each letter occurs. Case should be ignored. 
 
-sentence= input("Enter a sentence: ")
-sentence= sentence.lower()
-alphabet= "abcdefghijklmnopqrstuvwxyz"
-letter={}
-for char in sentence:
-  if char in alphabet:
-    letter[char]= letter[char]+1
-  else: 
-    letter[char]=1
+sentence = input("Enter a sentence: ")
 
-keys= letter.keys()
+sentence = sentence.lower() # convert to lowercase
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+letter = {} # empty dictionary
+for char in sentence:
+    if char in alphabet: # ignore any punctuation, numbers, etc
+        if char in letter:
+            letter[char] = letter[char] + 1
+        else:
+            letter[char] = 1
+
+keys = letter.keys()
 for char in sorted(keys):
-  print (char,letter[char])
+    print(char, letter[char])
