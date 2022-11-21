da229c707e8837614db1a5a5d0b97fd63453510c
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-11-21 14:11:24 +0000

    added reader demo

--- csv/csv-demo.py
+++ csv/csv-demo.py
@@ -1,5 +1,12 @@
-f= open ('demo.csv')
-for line in f.readlines():
-  print (line)
-  line= line.strip()
-  print(line.split(","))
+import csv
+#flawed split example
+#f= open ('demo.csv')
+#for line in f.readlines():
+  #print (line)
+  #line= line.strip()
+  #print(line.split(","))
+
+#example using csv module into a list
+reader=csv.reader(open("demo.csv"))
+for line in reader:
+  print(line)
