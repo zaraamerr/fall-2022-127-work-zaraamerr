15f73d7f612b560993ca5cd60d6500d5aed97843
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-12-12 16:51:54 +0000

    added data analysis to figure out how many ufo sightings per state

--- data/data.py
+++ data/data.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load the dataset into a Pandas dataframe
 df = pd.read_csv('ufo_sightings.csv')
+
+#how many ufo sightings per state?
+ufo_counts= df['Location.State'].value_counts()
+print (ufo_counts)
+#plot it
+
+# how many ufo sightings per city?
+# ufo_citycounts= df['Location.City'].value_counts()
+# print(ufo_citycounts)
