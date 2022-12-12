313bf7dc8060728ebff0a8c11112ca08a15e91cf
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-12-12 20:58:09 +0000

    added a second extra where i analyze another aspect (shapes of UFOs) of my source

--- data/data.py
+++ data/data.py
@@ -1,9 +1,12 @@
 #Data Project: Solo
 #Decription: Analysis of a CSV document containing information about UFO sightings throughout the US. I am analyzing how many UFO sightings occur per state, creating a bar graph to represent that data, and stating which state has the most UFO sightings. I am using PANDAS to analyze my data.
-#EXTRA 1: Use Matplotlib or another Python graphing/plotting library to visualize part or all of your analysis.
+#EXTRA 1: Use multiple aspects of a single data source in your analysis.
+#EXTRA 2: Use Matplotlib or another Python graphing/plotting library to visualize part or all of your analysis.
 
 import pandas as pd
 import matplotlib.pyplot as plt
+
+#BASE PROJECT
 
 # Load the dataset into a Pandas dataframe
 
@@ -11,7 +14,7 @@ df = pd.read_csv('ufo_sightings.csv')
 
 #check the column names of the df
 
-print (df.columns)
+df.columns
 
 #analyze how many ufo sightings per state?
 
@@ -24,7 +27,20 @@ max_state= ufo_counts.idxmax()
 max_sightings= ufo_counts.max()
 print("The state with the most UFO sightings is", max_state, "with " + str(max_sightings) + " UFO sightings.")
 
-#plot it
+#EXTRA 1: Use multiple aspects of a single data source in your analysis: For this, I am analyzing which shape most people used to describe the UFO as.
+
+#analyze which shape did most people describe the UFO as? 
+
+shape_counts= df['Data.Shape'].value_counts()
+print(shape_counts)
+
+#which shape is the most common description of the UFO?
+
+max_shape= shape_counts.idxmax()
+print("Most people described the UFO as the shape of a " + max_shape)
+
+#EXTRA 2: Use Matplotlib or another Python graphing/plotting library to visualize part or all of your analysis. For this, I am using matplotlib to graph the states and their respective total number of UFO sightings.
+
+#plot
 ufo_counts.plot(kind='bar')
 plt.show() #show the plot, the x values is the location/state and the y values are the number of ufo sightings. The bar graph aims to show how many UFO sightings occurred in each state.
-
