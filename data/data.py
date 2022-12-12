27596a08be677a8b52a4e356e8eb38a7a77eff01
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-12-12 19:22:58 +0000

    added graph plot for data

--- data/data.py
+++ data/data.py
@@ -2,13 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the dataset into a Pandas dataframe
+
 df = pd.read_csv('ufo_sightings.csv')
 
-#how many ufo sightings per state?
-ufo_counts= df['Location.State'].value_counts()
-print (ufo_counts)
-#plot it
+#check the column names of the df
 
-# how many ufo sightings per city?
-# ufo_citycounts= df['Location.City'].value_counts()
-# print(ufo_citycounts)
+print (df.columns)
+
+#analyze how many ufo sightings per state?
+
+ufo_counts= df['Location.City'].value_counts() #variable that counts sightings per state
+print (ufo_counts)
+
+#plot it
+ufo_counts.plot(kind='bar')
+plt.show() #show the plot, the x values is the location/state and the y values are the ufo sightings of that particular state.
