8840af4231c79b4cdc1e4b3d91e8d62a337f6ebd
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-12-12 20:28:48 +0000

    added code to determine which state had the most amt of ufo sightings

--- data/data.py
+++ data/data.py
@@ -1,3 +1,7 @@
+#Data Project: Solo
+#Decription: Analysis of a CSV document containing information about UFO sightings throughout the US. I am analyzing how many UFO sightings occur per state, creating a bar graph to represent that data, and stating which state has the most UFO sightings. I am using PANDAS to analyze my data.
+#EXTRA 1: Use Matplotlib or another Python graphing/plotting library to visualize part or all of your analysis.
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -11,9 +15,16 @@ print (df.columns)
 
 #analyze how many ufo sightings per state?
 
-ufo_counts= df['Location.City'].value_counts() #variable that counts sightings per state
+ufo_counts= df['Location.State'].value_counts() #variable that counts sightings per state
 print (ufo_counts)
+
+#which state has the most ufo sightings?
+
+max_state= ufo_counts.idxmax()
+max_sightings= ufo_counts.max()
+print("The state with the most UFO sightings is", max_state, "with " + str(max_sightings) + " UFO sightings.")
 
 #plot it
 ufo_counts.plot(kind='bar')
-plt.show() #show the plot, the x values is the location/state and the y values are the ufo sightings of that particular state.
+plt.show() #show the plot, the x values is the location/state and the y values are the number of ufo sightings. The bar graph aims to show how many UFO sightings occurred in each state.
+
