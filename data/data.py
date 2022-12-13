6745ff8a749ccdd348e57c182e38339f12eebb16
Zara Amer <110066582+zaraamerr@users.noreply.github.com>
2022-12-13 03:30:46 +0000

    minor improvements made. done.

--- data/data.py
+++ data/data.py
@@ -1,5 +1,5 @@
 #Data Project: Solo
-#Decription: Analysis of a CSV document containing information about UFO sightings throughout the US. I am analyzing how many UFO sightings occur per state, creating a bar graph to represent that data, stating which state has the most UFO sightings, and analyzing which shape most people describe the UFO as. I am using PANDAS to analyze my data.
+#Decription: Analysis of a CSV document containing information about UFO sightings throughout the US. Things I analyzed: how many UFO sightings occur per state, which state has the most UFO sightings, which state has the least UFO sightings, and what the most common UFO shape was. I am using PANDAS to analyze my data.
 #EXTRA 1: Use multiple aspects of a single data source in your analysis.
 #EXTRA 2: Use Matplotlib or another Python graphing/plotting library to visualize part or all of your analysis.
 
@@ -19,13 +19,19 @@ df.columns
 #analyze how many ufo sightings per state?
 
 ufo_counts= df['Location.State'].value_counts() #variable that counts sightings per state
-print (ufo_counts)
+print (ufo_counts) #prints each state and their total UFO sightings
 
 #which state has the most ufo sightings?
 
-max_state= ufo_counts.idxmax()
-max_sightings= ufo_counts.max()
+max_state= ufo_counts.idxmax() #determines the state with the most UFO sightings
+max_sightings= ufo_counts.max() #determines the number of said UFO sightings
 print("The state with the most UFO sightings is", max_state, "with " + str(max_sightings) + " UFO sightings.")
+
+#which state has the least ufo sightings?
+
+min_state= ufo_counts.idxmin() #determines the state with the least UFO sightings
+min_sightings= ufo_counts.min() #determines the number of said UFO sightings
+print("The state with the least UFO sightings is", min_state, "with " + str(min_sightings) + " UFO sightings.")
 
 #EXTRA 1: Use multiple aspects of a single data source in your analysis: For this, I am analyzing which shape most people used to describe the UFO as.
 
